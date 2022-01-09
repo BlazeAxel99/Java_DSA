db7e037024f07ab419d62cbc45827eef990b411c
KASH MOULIK <52042283+BlazeAxel99@users.noreply.github.com>
2022-01-10 02:41:02 +0530

    Create Find Pivot Index_leet.java

---
+++ Find Pivot Index_leet.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int pivotIndex(int[] arr) {
+        int ls = 0;
+        int rs =0;
+        for(int i=0;i<arr.length;i++){
+            rs+= arr[i];
+        }
+        for(int i=0;i<arr.length;i++){
+            rs-= arr[i];
+            if(rs==ls){
+                return i;
+            }
+            ls += arr[i];
+        }
+        return -1;
+        
+    }
+}
